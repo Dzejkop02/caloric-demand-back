b327c62ae0368ec2846383500ba40c7dfc558d60
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2022-07-06 21:32:28 +0200

    Added getData function and OneDay interface.

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { getDataResponse } from '../interfaces/data';
+import { DataService } from './data.service';
 
 @Controller('data')
-export class DataController {}
+export class DataController {
+  constructor(@Inject(DataService) private dataService: DataService) {}
+
+  @Get('/')
+  getData(): Promise<getDataResponse> {
+    return this.dataService.getData();
+  }
+}
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { getDataResponse } from '../interfaces/data';
+import { DataItem } from './data-item.entity';
 
 @Injectable()
-export class DataService {}
+export class DataService {
+  async getData(): Promise<getDataResponse> {
+    return DataItem.find();
+  }
+}
