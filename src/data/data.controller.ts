b59c83f2e660102a2fb70f894ea0a48349cd0bc8
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2022-07-06 22:19:38 +0200

    Added function that adds new day or updates if day already exists.

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { getDataResponse } from '../interfaces/data';
 import { DataService } from './data.service';
+import { UpdateDataDto } from './dto/update-data.dto';
 
 @Controller('data')
 export class DataController {
@@ -9,5 +10,10 @@ export class DataController {
   @Get('/')
   getData(): Promise<getDataResponse> {
     return this.dataService.getData();
+  }
+
+  @Post('/')
+  addDayOrUpdate(@Body() dayData: UpdateDataDto): Promise<getDataResponse> {
+    return this.dataService.addDayOrUpdate(dayData);
   }
 }
